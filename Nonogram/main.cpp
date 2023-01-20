f06a6f55da233c56aa5fb4a25af1c5743de5c39f
Atanas Hristov <the_doktora@abv.bg>
2023-01-20 23:57:07 +0200

    Addded comment block to main.cpp file.

--- Nonogram/main.cpp
+++ Nonogram/main.cpp
@@ -1,3 +1,19 @@
+/**
+*
+* Solution to course project # 4
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Atanas Hristov
+* @idnumber 8MI0600242
+* @compiler VC
+*
+* <maim cpp file>
+*
+*/
+
+
 #include "Functionality.h"
 
 int main() {
