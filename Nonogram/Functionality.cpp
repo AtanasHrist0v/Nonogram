641d35364baa6ff54b1b24dd48dcc6802a3c7c0a
Atanas Hristov <the_doktora@abv.bg>
2023-01-23 03:57:38 +0200

    Reworked Login logic with the .txt files. Now, allUsernames.txt is unnecessary. Also StringsAreEqual() and CopySting() are unnecessary.

--- Nonogram/Functionality.cpp
+++ Nonogram/Functionality.cpp
@@ -3,14 +3,16 @@
 
 const int LOGIN_MENU_LENGTH = 1;
 const int MAIN_MENU_LENGTH = 4;
-const int INPUT_MAX_LEGTH = 100;
+const int INPUT_MAX_LENGTH = 100;
 const int BUFFER_SIZE = 100;
 const int FIRST_LEVEL = 1;
 const int NUMBER_OF_LEVELS = 5;
 const int NONOGRAMS_PER_LEVEL = 2;
+const int TERMINATING_ZERO_LENGTH = 1;
 
 const char EXIT_CHAR = '3';
 const char ZERO_CHAR = '0';
+const char TERMINATING_ZERO_CHAR = '\0';
 const char NEW_USER_PROMPT[] = "Do you want to register user? [y/n]\n";
 const char REGISTER_PROMPT[] = "Enter the username you want to register: ";
 const char INVALID_INPUT_ERROR[] = "Error: Invalid input. Press Enter to continue.\n";
@@ -55,93 +57,131 @@ void DeallocateMenuMemory(const char** menu) {
 }
 
 const char** NewLoginMenu() {
-	const char** LOGIN_MENU = new const char* [LOGIN_MENU_LENGTH];
+	const char** LOGIN_MENU = new const char* [LOGIN_MENU_LENGTH] {};
 	LOGIN_MENU[0] = "Enter your username to login: ";
 
 	return LOGIN_MENU;
 }
 
-bool UsernameIsValid(const char* username) {
-	if (username == nullptr) {
+bool UsernameIsValid(const char* USERNAME) {
+	if (USERNAME == nullptr) {
 		std::cout << NULLPTR_ERROR;
 		return false;
 	}
 
-	if (username[0] == '\0') {
+	if (USERNAME[0] == TERMINATING_ZERO_CHAR) {
 		return false;
 	}
 
 	return true;
 }
 
-bool StringsAreEqual(const char* string1, int string1Length, const char* string2, int string2Length) {
-	if (string1 == nullptr || string2 == nullptr) {
+//unnecessary
+//bool StringsAreEqual(const char* string1, int string1Length, const char* string2, int string2Length) {
+//	if (string1 == nullptr || string2 == nullptr) {
+//		std::cout << NULLPTR_ERROR;
+//		return false;
+//	}
+//
+//	for (size_t i = 0; i < string1Length && i < string2Length; i++) {
+//		if (string1[i] != string2[i]) {
+//			return false;
+//		}
+//
+//		if (string1[i] == TERMINATING_ZERO_CHAR || string2[i] == TERMINATING_ZERO_CHAR) {
+//			break;
+//		}
+//	}
+//
+//	return true;
+//}
+
+int LengthOf(const char* string) {
+	if (string == nullptr) {
 		std::cout << NULLPTR_ERROR;
-		return false;
+		return -1;
 	}
 
-	for (size_t i = 0; i < string1Length && i < string2Length; i++) {
-		if (string1[i] != string2[i]) {
-			return false;
-		}
+	int length = 0;
 
-		if (string1[i] == '\0' || string2[i] == '\0') {
-			break;
-		}
+	while (string[length] != TERMINATING_ZERO_CHAR) {
+		length++;
 	}
 
-	return true;
+	return length;
 }
 
-bool UsernameExists(const char* username, int usernameLength) {
-	if (username == nullptr) {
+char* GetTxtPath(const char* PARENT_FOLDERS_PATH, const char* FILE_NAME, const char* FILE_EXTENTION) {
+	if (PARENT_FOLDERS_PATH == nullptr || FILE_NAME == nullptr || FILE_EXTENTION == nullptr) {
+		std::cout << NULLPTR_ERROR;
+		return nullptr;
+	}
+
+	char* userTxtPath = new char[LengthOf(PARENT_FOLDERS_PATH) + LengthOf(FILE_NAME) + LengthOf(FILE_EXTENTION) + TERMINATING_ZERO_LENGTH] {};
+	int currentIndex = 0;
+
+	for (size_t i = 0; PARENT_FOLDERS_PATH[i] != TERMINATING_ZERO_CHAR; i++, currentIndex++) {
+		userTxtPath[currentIndex] = PARENT_FOLDERS_PATH[i];
+	}
+	for (size_t i = 0; FILE_NAME[i] != TERMINATING_ZERO_CHAR; i++, currentIndex++) {
+		userTxtPath[currentIndex] = FILE_NAME[i];
+	}
+	for (size_t i = 0; FILE_EXTENTION[i] != TERMINATING_ZERO_CHAR; i++, currentIndex++) {
+		userTxtPath[currentIndex] = FILE_EXTENTION[i];
+	}
+
+	return userTxtPath;
+}
+
+char* GetUserTxtPath(const char* USERNAME) {
+	if (USERNAME == nullptr) {
+		std::cout << NULLPTR_ERROR;
+		return nullptr;
+	}
+
+	char* userTxtPath = GetTxtPath(USERNAME_PARENT_FOLDER, USERNAME, TEXT_FILE_EXTENTION);
+
+	return userTxtPath;
+}
+
+bool UsernameExists(const char* USERNAME) {
+	if (USERNAME == nullptr) {
 		std::cout << NULLPTR_ERROR;
 		return false;
 	}
 
-	std::ifstream reader(ALL_USERNAMES_PATH);
+	char* userTxtPath = GetUserTxtPath(USERNAME);
 
-	if (!reader.is_open()) {
-		std::ofstream writer(ALL_USERNAMES_PATH);
+	std::ifstream reader(userTxtPath);
 
-		if (!writer.is_open()) {
-			std::cout << COULDNT_WRITE_TO_FILE_ERROR;
-			return false;
-		}
+	delete[] userTxtPath;
 
-		writer.close();
-		reader.clear();
+	if (reader.is_open()) {
+		reader.close();
+		return true;
 	}
 
-	char buffer[BUFFER_SIZE]{};
-	while (!reader.eof()) {
-		reader.getline(buffer, BUFFER_SIZE);
-
-		if (StringsAreEqual(username, usernameLength, buffer, BUFFER_SIZE)) {
-			reader.close();
-			return true;
-		}
-	}
-
-	reader.close();
 	return false;
 }
 
-void RegisterUser(const char* username) {
-	if (username == nullptr) {
+void RegisterUser(const char* USERNAME) {
+	if (USERNAME == nullptr) {
 		std::cout << NULLPTR_ERROR;
 		return;
 	}
 
-	std::ofstream writer(ALL_USERNAMES_PATH, std::ios::app);
+	char* userTxtPath = GetUserTxtPath(USERNAME);
+
+	std::ofstream writer(userTxtPath);
 
 	if (!writer.is_open()) {
 		std::cout << COULDNT_WRITE_TO_FILE_ERROR;
 		return;
 	}
 
-	writer << username << std::endl;
 	writer.close();
+
+	delete[] userTxtPath;
 }
 
 void CopyString(const char* source, int sourceLength, char* destination, int destinationLength) {
@@ -155,23 +195,22 @@ void CopyString(const char* source, int sourceLength, char* destination, int des
 	}
 }
 
-void LoginMenu(char* username) {
+void LoginMenu(char* USERNAME) {
 	const char** LOGIN_MENU = NewLoginMenu();
-	char userInput[INPUT_MAX_LEGTH]{};
-	char newUserPromptUserInput[INPUT_MAX_LEGTH]{};
+	char userInput[INPUT_MAX_LENGTH]{};
 	bool usernameExists = false;
 
 	do {
 		DisplayMenu(LOGIN_MENU, LOGIN_MENU_LENGTH);
-		std::cin.getline(userInput, 100);
+		std::cin.getline(USERNAME, 100);
 
-		if (!UsernameIsValid(userInput)) {
+		if (!UsernameIsValid(USERNAME)) {
 			std::cout << INVALID_INPUT_ERROR;
-			std::cin.getline(userInput, 100);
+			std::cin.getline(USERNAME, 100); //If not for this get getline(), DisplayMenu() would clear the console and the user won't see that his input is invalid.
 			continue;
 		}
 
-		usernameExists = UsernameExists(userInput, INPUT_MAX_LEGTH);
+		usernameExists = UsernameExists(USERNAME);
 
 		if (usernameExists) {
 			break;
@@ -179,76 +218,28 @@ void LoginMenu(char* username) {
 
 		std::cout << LOGIN_ERROR;
 		std::cout << NEW_USER_PROMPT;
-		std::cin.getline(newUserPromptUserInput, 100);
+		std::cin.getline(userInput, 100);
 
-		if (newUserPromptUserInput[0] == 'y' && newUserPromptUserInput[1] == '\0') {
-			RegisterUser(userInput);
+		if (userInput[0] == 'y' && userInput[1] == TERMINATING_ZERO_CHAR) {
+			RegisterUser(USERNAME);
 			usernameExists = true;
 		}
 
 	} while (!usernameExists);
 
-	CopyString(userInput, INPUT_MAX_LEGTH, username, INPUT_MAX_LEGTH);
+	//CopyString(userInput, INPUT_MAX_LENGTH, USERNAME, INPUT_MAX_LENGTH);
 
 	DeallocateMenuMemory(LOGIN_MENU);
 }
 
-const char** NewMainMenu(const char* username) {
+const char** NewMainMenu(const char* USERNAME) {
 	const char** MAIN_MENU = new const char* [MAIN_MENU_LENGTH];
-	MAIN_MENU[0] = username;
+	MAIN_MENU[0] = USERNAME;
 	MAIN_MENU[1] = "\n\n1) Continue\n";
 	MAIN_MENU[2] = "2) Start new Nonogram\n";
 	MAIN_MENU[3] = "3) Exit\n";
 
 	return MAIN_MENU;
-}
-
-int LengthOf(const char* string) {
-	if (string == nullptr) {
-		std::cout << NULLPTR_ERROR;
-		return -1;
-	}
-
-	int length = 0;
-
-	for (size_t i = 0; string[i] != '\0'; i++) {
-		length++;
-	}
-
-	return length;
-}
-
-char* GetTxtPath(const char* PARENT_FOLDERS_PATH, const char* FILE_NAME, const char* FILE_EXTENTION) {
-	if (PARENT_FOLDERS_PATH == nullptr || FILE_NAME == nullptr || FILE_EXTENTION == nullptr) {
-		std::cout << NULLPTR_ERROR;
-		return nullptr;
-	}
-
-	char* userTxtPath = new char[LengthOf(PARENT_FOLDERS_PATH) + LengthOf(FILE_NAME) + LengthOf(FILE_EXTENTION) + 1] {};
-	int currentIndex = 0;
-
-	for (size_t i = 0; PARENT_FOLDERS_PATH[i] != '\0'; i++, currentIndex++) {
-		userTxtPath[currentIndex] = PARENT_FOLDERS_PATH[i];
-	}
-	for (size_t i = 0; FILE_NAME[i] != '\0'; i++, currentIndex++) {
-		userTxtPath[currentIndex] = FILE_NAME[i];
-	}
-	for (size_t i = 0; FILE_EXTENTION[i] != '\0'; i++, currentIndex++) {
-		userTxtPath[currentIndex] = FILE_EXTENTION[i];
-	}
-
-	return userTxtPath;
-}
-
-char* GetUserTxtPath(const char* username) {
-	if (username == nullptr) {
-		std::cout << NULLPTR_ERROR;
-		return nullptr;
-	}
-
-	char* userTxtPath = GetTxtPath(USERNAME_PARENT_FOLDER, username, TEXT_FILE_EXTENTION);
-
-	return userTxtPath;
 }
 
 inline char IntToChar(int number) {
@@ -288,13 +279,13 @@ inline char IntToChar(int number) {
 //	delete[] currentLevelPath;
 //}
 
-void ContinueLastNonogram(const char* username) {
-	if (username == nullptr) {
+void ContinueLastNonogram(const char* USERNAME) {
+	if (USERNAME == nullptr) {
 		std::cout << NULLPTR_ERROR;
 		return;
 	}
 
-	char* userTxtPath = GetUserTxtPath(username);
+	char* userTxtPath = GetUserTxtPath(USERNAME);
 
 	std::ifstream reader(userTxtPath);
 
@@ -322,8 +313,8 @@ void ContinueLastNonogram(const char* username) {
 	delete[] userTxtPath;
 }
 
-void StartNewNonogram(const char* username) {
-	if (username == nullptr) {
+void StartNewNonogram(const char* USERNAME) {
+	if (USERNAME == nullptr) {
 		std::cout << NULLPTR_ERROR;
 		return;
 	}
@@ -347,14 +338,14 @@ bool UserInputIsCorrect(const char* userInput, int menuLength) {
 	}
 }
 
-char MainMenu(const char* username) {
-	if (username == nullptr) {
+char MainMenu(const char* USERNAME) {
+	if (USERNAME == nullptr) {
 		std::cout << NULLPTR_ERROR;
-		return '\0';
+		return TERMINATING_ZERO_CHAR;
 	}
 
-	const char** MAIN_MENU = NewMainMenu(username);
-	char userInput[INPUT_MAX_LEGTH]{};
+	const char** MAIN_MENU = NewMainMenu(USERNAME);
+	char userInput[INPUT_MAX_LENGTH]{};
 
 	do {
 		DisplayMenu(MAIN_MENU, MAIN_MENU_LENGTH);
@@ -367,18 +358,18 @@ char MainMenu(const char* username) {
 	return userInput[0];
 }
 
-void StartGame(const char* username) {
-	char mainMenuChoice = '\0';
+void StartGame(const char* USERNAME) {
+	char mainMenuChoice = TERMINATING_ZERO_CHAR;
 
 	do {
-		mainMenuChoice = MainMenu(username);
+		mainMenuChoice = MainMenu(USERNAME);
 
 		switch (mainMenuChoice) {
 			case '1':
-				ContinueLastNonogram(username);
+				ContinueLastNonogram(USERNAME);
 				break;
 			case '2':
-				StartNewNonogram(username);
+				StartNewNonogram(USERNAME);
 				break;
 			default:
 				break;
@@ -386,13 +377,13 @@ void StartGame(const char* username) {
 	} while (mainMenuChoice != EXIT_CHAR);
 
 	ClearConsole();
-	std::cout << "Have a great day, " << username << "!\n";
+	std::cout << "Have a great day, " << USERNAME << "!\n";
 }
 
 void StartProgram() {
 	srand(time(0));
 
-	char* username = new char[INPUT_MAX_LEGTH] {};
+	char* username = new char[INPUT_MAX_LENGTH] {};
 
 	LoginMenu(username);
 	StartGame(username);
